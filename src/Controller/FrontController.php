b6c1e98132553c0fe92eeacd20a05c806874bd65
Tom-Dln <tom.delaunay.perso@gmail.com>
2022-04-17 20:20:58 +0200

    Système de Pagination

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Repository\ItemRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,10 +14,16 @@ class FrontController extends AbstractController
     /**
      * @Route("/", name="app_front")
      */
-    public function index(ItemRepository $itemRepository): Response
+    public function index(ItemRepository $itemRepository, Request $request, PaginatorInterface $paginatorInterface): Response
     {
         $items = $itemRepository->findAll();
         dump($items);
+        $items = $paginatorInterface->paginate(
+            $items, /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            6 /*limit per page*/
+        );
+        
 
         return $this->render('front/index.html.twig', [
             'items' => $items,
