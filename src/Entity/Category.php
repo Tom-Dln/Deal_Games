6a2641e94db0746cec628068539156e25afbd192
Tom-Dln <tom.delaunay.perso@gmail.com>
2022-04-17 15:48:15 +0200

    Précréation Formulaire d'ajout Annonce

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -34,6 +34,11 @@ class Category
         $this->items = new ArrayCollection();
     }
 
+    public function __toString()
+    {
+        return $this->getTitle();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
