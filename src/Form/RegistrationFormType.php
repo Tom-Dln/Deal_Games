94ea66cc3f7f765a9acf216b49273986d718dc77
Tom-Dln <tom.delaunay.perso@gmail.com>
2022-04-20 09:48:24 +0200

    Réinitialisation MDP & Corrections Inscription

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -30,11 +30,11 @@ class RegistrationFormType extends AbstractType
                 'label' => 'Adresse email',
             ])
             ->add('agreeTerms', CheckboxType::class, [
-                'label' => 'J\'accepte les Conditions Générales d\'Utilisation et la Politique de Confidentialité',
+                'label' => 'J\'accepte les Conditions Générales d\'Utilisation et la Politique de Confidentialité.',
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                        'message' => 'Vous devez accepter les Conditions Générales d\'Utilisation et la Politique de Confidentialité.',
                     ]),
                 ],
             ])
@@ -46,11 +46,11 @@ class RegistrationFormType extends AbstractType
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Veuillez entrer un mot de passe.',
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => 'Votre mot de passe doit comporter au moins {{ limit }} caractères.',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
